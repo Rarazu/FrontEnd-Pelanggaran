49f1a1351e5b4cdbd9e20d8e1fd1b5431e6844d0
Rarazu <rara_zahra_29rpl@student.smktelkom-mlg.sch.id>
2022-04-26 12:02:54 +0700

    Update Siswa Page

--- src/pages/Siswa.js
+++ src/pages/Siswa.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Modal, Toast } from "bootstrap";
 
-export default function Siswa(){
+export default function Siswa() {
     let [siswa, setSiswa] = useState([])
     let [idSiswa, setIdSiswa] = useState(0)
     let [nis, setNis] = useState(0)
@@ -14,6 +14,7 @@ export default function Siswa(){
 
     let [message, setMessage] = useState("")
     let [modal, setModal] = useState(null)
+    let [uploadImage, setUploadImage] = useState(true)
 
     // prepare token
     let token = localStorage.getItem(`token-pelanggaran`)
@@ -29,11 +30,11 @@ export default function Siswa(){
 
         // sending data
         axios.get(endpoint, authorization)
-        .then(response => {
-            // simpan ke state siswa
-            setSiswa(response.data)
-        })
-        .catch(error => console.log(error))
+            .then(response => {
+                // simpan ke state siswa
+                setSiswa(response.data)
+            })
+            .catch(error => console.log(error))
     }
 
     /** create function to show Toast */
@@ -60,6 +61,82 @@ export default function Siswa(){
         setPoin(0)
         setImage(null)
         setAction("insert")
+        setUploadImage(true)
+    }
+
+    let editSiswa = item => {
+        modal.show()
+        setIdSiswa(item.id_siswa)
+        setNis(item.nis)
+        setNama(item.nama)
+        setKelas(item.kelas)
+        setPoin(item.poin)
+        setImage(null)
+        setAction("edit")
+        setUploadImage(false)
+    }
+
+    let saveSiswa = ev => {
+        ev.preventDefault()
+
+        // close modal
+        modal.hide()
+
+        if (action === `insert`) {
+            /** 
+             * 1. mengirimkan ke backend
+             * 2. isi request
+             * 3. sisipkan data menggunankan formData
+             */
+            let endpoint = `http://localhost:8000/siswa`
+            let request = new FormData()
+            request.append(`nis`, nis)
+            request.append(`nama`, nama)
+            request.append(`kelas`, kelas)
+            request.append(`poin`, poin)
+            request.append(`image`, image)
+
+            // sending data
+            axios.post(endpoint, request, authorization)
+                .then(response => {
+                    showToast(response.data.message)
+                    // refresh newest data
+                    getData()
+                })
+                .catch(error => console.log(error))
+        } else if (action === `edit`) {
+            let endpoint = `http://localhost:8000/siswa/${idSiswa}`
+            let request = new FormData()
+            request.append(`nis`, nis)
+            request.append(`nama`, nama)
+            request.append(`kelas`, kelas)
+            request.append(`poin`, poin)
+            
+            if (uploadImage === true) {
+                request.append(`image`, image)
+            }
+
+            // sending data
+            axios.put(endpoint, request, authorization)
+                .then(response => {
+                    showToast(response.data.message)
+                    getData()
+                })
+                .catch(error => console.log(error))
+        }
+    }
+
+    let deleteSiswa = item => {
+        if (window.confirm(`Reolly?? You Want to Delete THIS?`)) {
+            let endpoint = `http://localhost:8000/siswa/${item.id_siswa}`
+            // sending data
+            axios.delete(endpoint, authorization)
+                .then(response => {
+                    showToast(response.data.message)
+                    getData()
+                })
+                .catch(error => console.log(error))
+        }
     }
 
     // memanggil getData
@@ -71,7 +148,7 @@ export default function Siswa(){
         getData()
     }, [])
 
-    return(
+    return (
         <div className="container-fluid">
 
             {/* Start Toast Component */}
@@ -89,51 +166,56 @@ export default function Siswa(){
             {/* End Toast Component */}
 
             <div className="card m-3">
-                <div className="card-header" style={{background: `teal`}}>
-                    <h3 style={{color: `lightcyan`}}>
+                <div className="card-header" style={{ background: `teal` }}>
+                    <h3 style={{ color: `lightcyan` }}>
                         Daftar Siswa
                     </h3>
                 </div>
-                <div className="card-body" style={{background: `lightblue`}}>
+                <div className="card-body" style={{ background: `lightblue` }}>
                     <ul className="list-group">
                         {siswa.map(item => (
                             <li className="list-group-item"
-                            key={`key-${item.id_siswa}`}>
+                                key={`key-${item.id_siswa}`}>
                                 <div className="row">
                                     {/* gambar */}
                                     <div className="col-lg-4">
-                                        <img src={`http://localhost:8000/image/${item.image}`} 
-                                        alt="Sabar..gambarnya error"
-                                        style={{width: `250px`, height: `250px`, borderRadius: `50%`}} />
+                                        <img src={`http://localhost:8000/image/${item.image}`}
+                                            alt="Sabar..gambarnya error"
+                                            style={{ width: `250px`, height: `250px`, borderRadius: `50%` }} />
                                     </div>
                                     {/* deskripsi */}
-                                    <div className="col-lg-4">
-                                        <small className="fst-italic" style={{color: `teal`}}>
-                                           NIS
+                                    <div className="col-lg-5">
+                                        <small className="fst-italic" style={{ color: `teal` }}>
+                                            NIS
                                         </small>
                                         <h5>{item.nis}</h5>
-                                        <small className="fst-italic" style={{color: `teal`}}>
+                                        <small className="fst-italic" style={{ color: `teal` }}>
                                             Nama Siswa
                                         </small>
                                         <h5>{item.nama}</h5>
-                                        <small className="fst-italic" style={{color: `teal`}}>
+                                        <small className="fst-italic" style={{ color: `teal` }}>
                                             Kelas
                                         </small>
                                         <h5>{item.kelas}</h5>
-                                        <small className="fst-italic" style={{color: `teal`}}>
+                                        <small className="fst-italic" style={{ color: `teal` }}>
                                             Poin
                                         </small>
                                         <h5>{item.poin}</h5>
-                                        <small className="fst-italic" style={{color: `teal`}}>
-                                            Option
-                                        </small>
-                                        <br/>
-                                        <button className="btn btn-info btn-sm m-2">
-                                            Edit
-                                        </button>
-                                        <button className="btn btn-danger btn-sm m-2">
-                                            Delete
-                                        </button>
+                                    </div>
+                                    <div className="col-lg-3">
+                                        <div className="d-grid gap-2">
+                                            <small className="fst-italic text-center" style={{ color: `teal` }}>
+                                                Option
+                                            </small>
+                                            <button className="btn btn-info btn-sm m-2"
+                                                onClick={() => editSiswa(item)}>
+                                                <span className="fa fa-edit"></span> Edit
+                                            </button>
+                                            <button className="btn btn-danger btn-sm m-2"
+                                            onClick={() => deleteSiswa(item)}>
+                                                <span className="fa fa-trash"></span> Delete
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </li>
@@ -141,7 +223,7 @@ export default function Siswa(){
                     </ul>
                     {/* button add data siswa */}
                     <button className="btn btn-success btn-sm m-2"
-                    onClick={() => addSiswa()}>
+                        onClick={() => addSiswa()}>
                         <span className="fa fa-plus"></span> Add Data
                     </button>
 
@@ -149,37 +231,48 @@ export default function Siswa(){
                     <div className="modal" id="modal_siswa">
                         <div className="modal-dialog">
                             <div className="modal-content">
-                                <div className="modal-header" style={{background: `lightblue`}}>
+                                <div className="modal-header" style={{ background: `lightblue` }}>
                                     <h4 className="text-secondary">
                                         Form Siswa
                                     </h4>
                                 </div>
                                 <div className="modal-body">
-                                    <form>
+                                    <form onSubmit={(ev) => saveSiswa(ev)}>
                                         {/* input data */}
                                         NIS
                                         <input type="number" className="form-control mb-2" required
-                                        value={nis} onChange={ev => setNis(ev.target.value)}/>
+                                            value={nis} onChange={ev => setNis(ev.target.value)} />
 
                                         Nama
                                         <input type="text" className="form-control mb-2" required
-                                        value={nama} onChange={ev => setNama(ev.target.value)}/>
+                                            value={nama} onChange={ev => setNama(ev.target.value)} />
 
                                         Kelas
                                         <input type="text" className="form-control mb-2" required
-                                        value={kelas} onChange={ev => setKelas(ev.target.value)}/>
+                                            value={kelas} onChange={ev => setKelas(ev.target.value)} />
 
                                         Poin
                                         <input type="number" className="form-control mb-2" required
-                                        value={poin} onChange={ev => setPoin(ev.target.value)}/>
+                                            value={poin} onChange={ev => setPoin(ev.target.value)} />
 
                                         Gambar
-                                        <input type="file" className="form-control mb-2" required
-                                        accept="image/*"
-                                        onChange={ev => setImage(ev.target.files[0])}/>
-                                        
+                                        <input type="file" 
+                                            className={`form-control mb-2 ${uploadImage ? `` : `d-none`}`}
+                                            required={uploadImage}
+                                            accept="image/*"
+                                            onChange={ev => setImage(ev.target.files[0])} />
+
+                                        <br/>
+                                        <button className={`btn btn-sm my-2 ${uploadImage ? `d-none` : ``}`}
+                                        type="button"
+                                        style={{background: `lightgray`}}
+                                        onClick={() => setUploadImage(true)}>
+                                            Click to re-upload image
+                                        </button>
+
+                                        <br/>
                                         {/* button  */}
-                                        <button className="btn btn-success" type="submit">
+                                        <button className="btn btn-success mx-1" type="submit">
                                             <span className="fa fa-check"></span> Save
                                         </button>
                                         <button type="button" className="btn btn-secondary mx-1" data-bs-dismiss="modal">
@@ -190,6 +283,7 @@ export default function Siswa(){
                             </div>
                         </div>
                     </div>
+                    {/* end of modal */}
                 </div>
             </div>
         </div>
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,10 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Toast } from "bootstrap";
+// import { Alert } from "bootstrap";
 import { Modal } from "bootstrap";
 
 export default function User() {
     let [user, setUser] = useState([])
+    let [modal, setModal] = useState(null)
+
+    let [idUser, setIdUser] = useState(0)
+    let [nama, setNama] = useState("")
+    let [username, setUsername] = useState("")
+    let [password, setPassword] = useState("")
+    let [action, setAction] = useState("")
+    let [message, setMessage] = useState("")
+
     let token = localStorage.getItem(`token-pelanggaran`)
 
     let authorization = {
@@ -23,12 +33,139 @@ export default function User() {
             .catch(error => console.log(error))
     }
 
+    /** create function to show Toast */
+    let showToast = message => {
+        let myToast = new Toast(
+            document.getElementById(`myToast`),
+            {
+                autohide: true
+            }
+        )
+        /** perintah untuk mengisi state 'message' */
+        setMessage(message)
+        /** tampilkan toast */
+        myToast.show()
+    }
+
+
+    // let showAlert = message => {
+    //     let myAlert = new Alert(
+    //         document.getElementById(`myAlert`),
+    //         {
+    //             autohide: true
+    //         }
+    //     )
+    //     setMessage(message)
+    //     myAlert.show()
+    // }
+
+    let addUser = () => {
+        modal.show()
+        setIdUser(0)
+        setNama("")
+        setUsername("")
+        setPassword("")
+        setAction("insert")
+    }
+
+    let editUser = item => {
+        modal.show()
+
+        setIdUser(item.id_user)
+        setNama(item.nama_user)
+        setUsername(item.username)
+        setPassword(item.password)
+        setAction('edit')
+    }
+
+    let saveUser = ev => {
+        modal.hide()
+
+        if (action === `insert`) {
+            let endpoint = `http://localhost:8000/user`
+            let request = {
+                nama_user: nama,
+                username: username,
+                password: password
+            }
+
+            axios.post(endpoint, request, authorization)
+                .then(response => {
+                    showToast(response.data.message)
+                    getData()
+                })
+                .catch(error => console.log(error))
+        } else if (action === `edit`) {
+            let endpoint = `http://localhost:8000/user/${idUser}`
+            let request = {
+                nama_user: nama,
+                username: username,
+                password: password
+            }
+
+            axios.put(endpoint, request, authorization)
+            .then(response => {
+                showToast(response.data.message)
+                getData()
+            })
+            .catch(error => console.log(error))
+        }
+    }
+
+    let deleteUser = item => {
+        if (window.confirm(`Are you sure ?`)) {
+            let endpoint = `http://localhost:8000/user/${item.id_user}`
+
+            axios.delete(endpoint, authorization)
+            .then(response => {
+                showToast(response.data.message)
+                getData()
+            })
+            .catch(error => console.log(error))
+        }
+    }
+
+    // var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    // var alertTrigger = document.getElementById('liveAlertBtn')
+
+    // function alert(message, type) {
+    //     var wrapper = document.createElement('div')
+    //     wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-coreui-dismiss="alert" aria-label="Close"></button></div>'
+
+    //     alertPlaceholder.append(wrapper)
+    // }
+
+    // if (alertTrigger) {
+    //     alertTrigger.addEventListener('click', function () {
+    //         alert('Nice, you triggered this alert message!', 'success')
+    //     })
+    // }
+
     useEffect(() => {
+        let myModal = new Modal(
+            document.getElementById("modal_user")
+        )
+        setModal(myModal)
         getData()
     }, [])
 
     return (
         <div className="container-fluid">
+
+            {/* Start Toast Component */}
+            <div className="position-fixed top-0 end-0 p-3"
+                style={{ zIndex: 11 }}>
+                <div className="toast bg-light" id="myToast">
+                    <div className="toast-header text-white" style={{ background: `teal` }}>
+                        <strong>Message</strong>
+                    </div>
+                    <div className="toast-body">
+                        {message}
+                    </div>
+                </div>
+            </div>
+            {/* End Toast Component */}
+
             <div className="card m-3">
                 <div className="card-header" style={{ background: `lightgray` }}>
                     <h3 style={{ color: `teal` }}>
@@ -62,11 +199,13 @@ export default function User() {
                                         <small className="fst-italic" style={{ color: `teal` }}>
                                             Option
                                         </small>
-                                        <br/>
-                                        <button className="btn btn-sm btn-info mx-1">
+                                        <br />
+                                        <button className="btn btn-sm btn-info mx-1"
+                                        onClick={() => editUser(item)}>
                                             <span className="fa fa-edit"></span> Edit
                                         </button>
-                                        <button className="btn btn-sm btn-danger mx-1">
+                                        <button className="btn btn-sm btn-danger mx-1"
+                                        onClick={() => deleteUser(item)}>
                                             <span className="fa fa-trash"></span> Delete
                                         </button>
                                     </div>
@@ -76,10 +215,51 @@ export default function User() {
                     </ul>
 
                     {/* button add user */}
-                    <button className="btn btn-sm btn-success my-3">
+                    <button className="btn btn-sm btn-success my-3"
+                        onClick={() => addUser()}>
                         <span className="fa fa-plus"></span>
                         Add User
                     </button>
+
+                    {/* modal add user */}
+                    <div className="modal" id="modal_user">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header" style={{ background: `lightcyan`, color: 'teal' }}>
+                                    <h4>
+                                        Form User
+                                    </h4>
+                                </div>
+                                <div className="modal-body">
+                                    <form onSubmit={(ev) => saveUser(ev)}>
+                                        Name
+                                        <input type="text" className="form-control mb-2" required
+                                            value={nama} onChange={ev => setNama(ev.target.value)} />
+
+                                        Username
+                                        <input type="text" className="form-control mb-2" required
+                                            value={username} onChange={ev => setUsername(ev.target.value)} />
+
+                                        Password
+                                        <input type="password" className="form-control mb-2" required
+                                            value={password} onChange={ev => setPassword(ev.target.value)} />
+
+                                        <button className="btn btn-success mx-1" type="submit">
+                                            <span className="fa fa-check"></span> Save
+                                        </button>
+                                        <button type="button" className="btn btn-secondary mx-1" data-bs-dismiss="modal">
+                                            <span className="fa fa-times"></span> Cancel
+                                        </button>
+
+                                        {/* <div id="liveAlertPlaceholder"></div>
+                                        <button type="button" class="btn btn-primary" id="liveAlertBtn">Show live alert</button> */}
+                                        
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* end of modal */}
 
                 </div>
             </div>
